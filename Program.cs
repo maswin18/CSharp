5b79fe2b8ff2962ed6d62ea8b29c44b85b4c96dd
maswin18 <280404051+maswin18@users.noreply.github.com>
2026-05-08 16:13:07 -0400

    add product class and object creation

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@ Console.WriteLine($"Name: {employee.Name}");
 Console.WriteLine($"Age: {employee.Age}");
 employee.Introduce();
 
+Product product = new Product("Keyboard", 49.99f);
+
+Console.WriteLine($"Product: {product.Name}");
+Console.WriteLine($"Price: {product.Price}");
+
 class Employee
 {
     public string? Name { get; set; }
@@ -19,5 +24,18 @@ class Employee
     public void Introduce()
     {
         Console.WriteLine($"Hello my name is {Name}");
+    }
+}
+
+class Product
+{
+    public string? Name { get; set; }
+
+    public float Price { get; set; }
+
+    public Product(string name, float price)
+    {
+        Name = name;
+        Price = price;
     }
 }
