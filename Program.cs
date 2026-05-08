ccf76686f488721da0bf7b423dbea03c41afab58
maswin18 <280404051+maswin18@users.noreply.github.com>
2026-05-08 14:44:21 -0400

    add conditional logic

--- Program.cs
+++ Program.cs
@@ -1,18 +1,13 @@
-﻿Console.WriteLine("Enter first number:");
-string? firstInput = Console.ReadLine();
+﻿Console.WriteLine("Enter your age:");
+string? input = Console.ReadLine();
 
-Console.WriteLine("Enter second number:");
-string? secondInput = Console.ReadLine();
+int age = Convert.ToInt32(input);
 
-int firstNumber = Convert.ToInt32(firstInput);
-int secondNumber = Convert.ToInt32(secondInput);
-
-int addition = firstNumber + secondNumber;
-int subtraction = firstNumber - secondNumber;
-int multiplication = firstNumber * secondNumber;
-int division = firstNumber / secondNumber;
-
-Console.WriteLine($"Addition: {addition}");
-Console.WriteLine($"Subtraction: {subtraction}");
-Console.WriteLine($"Multiplication: {multiplication}");
-Console.WriteLine($"Division: {division}");
+if (age >= 18)
+{
+    Console.WriteLine("adult");
+}
+else
+{
+    Console.WriteLine("Underage");
+}
