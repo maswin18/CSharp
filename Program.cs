fe853f78459b1ae5eee77e10a0c5482203aae1e8
maswin18 <280404051+maswin18@users.noreply.github.com>
2026-05-08 14:23:15 -0400

    add console input examples

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string firstName = "Maswin";
+string lastName = "Putra";
+
+int age = 42;
+double height = 178;
+
+bool isWorking = true;
+
+Console.WriteLine($"First Name: {firstName}");
+Console.WriteLine($"Last Name: {lastName}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Height: {height}");
+Console.WriteLine($"Working Status: {isWorking}");
