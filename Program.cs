bf63cd8eafb2c110f353df4249657d5068b7f91b
maswin18 <280404051+maswin18@users.noreply.github.com>
2026-05-08 14:42:27 -0400

    add arithmetic operators

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
-﻿Console.WriteLine("Enter your name:");
-string? name = Console.ReadLine();
+﻿Console.WriteLine("Enter first number:");
+string? firstInput = Console.ReadLine();
 
-Console.WriteLine("Enter your age:");
-string? age = Console.ReadLine();
+Console.WriteLine("Enter second number:");
+string? secondInput = Console.ReadLine();
 
-Console.WriteLine($"Name: {name}");
-Console.WriteLine($"Age: {age}");
+int firstNumber = Convert.ToInt32(firstInput);
+int secondNumber = Convert.ToInt32(secondInput);
+
+int addition = firstNumber + secondNumber;
+int subtraction = firstNumber - secondNumber;
+int multiplication = firstNumber * secondNumber;
+int division = firstNumber / secondNumber;
+
+Console.WriteLine($"Addition: {addition}");
+Console.WriteLine($"Subtraction: {subtraction}");
+Console.WriteLine($"Multiplication: {multiplication}");
+Console.WriteLine($"Division: {division}");
