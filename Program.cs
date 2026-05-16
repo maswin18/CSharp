41220dfc67e78fc8730c42950c3e521cb0735033
maswin18 <280404051+maswin18@users.noreply.github.com>
2026-05-16 10:14:22 -0400

    implement async and await basics

--- Program.cs
+++ Program.cs
@@ -1,49 +1,10 @@
-﻿using CSharp.Models;
+﻿string result = await GetMessage();
 
-List<Employee> employees = new List<Employee>()
+Console.WriteLine(result);
+
+async Task<string> GetMessage()
 {
-    new Employee("Maswin", 30, "IT"),
-    new Employee("John", 40, "HR"),
-    new Employee("Sarah", 25, "IT"),
-    new Employee("Michael", 35, "Finance")
-};
+    await Task.Delay(2000);
 
-/*List<Employee> itEmployees = employees
-    .Where(employee => employee.Department == "IT")
-    .ToList();
-
-foreach(Employee employee in itEmployees)
-{
-    Console.WriteLine(employee.Name);
-}
-
-
-List<Employee> orderEmployees = employees
-    .OrderBy(employee => employee.Age)
-    .ToList();
-
-foreach (Employee employee in orderEmployees)
-{
-    Console.WriteLine($"{employee.Name} - {employee.Age}");
-}
-
-
-List<string?> names = employees
-    .Select(employee => employee.Name)
-    .ToList();
-
-foreach (string? name in names)
-{
-    Console.WriteLine(name);
-}
-
-*/
-
-Employee? employee = employees
-    .FirstOrDefault(employee => employee.Name == "Sarah");
-
-if (employee != null)
-{
-    Console.WriteLine(employee.Name);
-    Console.WriteLine(employee.Department);
+    return "Hello from async method";
 }
